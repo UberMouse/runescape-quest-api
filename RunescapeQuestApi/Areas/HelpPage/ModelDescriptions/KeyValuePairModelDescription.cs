9b0779ff92ba6100ca64d47bf5ceb20b876a0c2f
UberMouse <UberMouse@UBERMOUSE-PC>
2017-02-26 13:33:04 +1300

    Add project files.

---
+++ RunescapeQuestApi/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace RunescapeQuestApi.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
---
+++ RunescapeQuestApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RunescapeQuestApi.WebApiApplication" Language="C#" %>
